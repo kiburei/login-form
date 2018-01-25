37caabe38b85fb28810e7b9249a4a1147728976f
kiburei <akiburei@gmail.com>
2018-01-25 14:00:58 +0300

    Optimize form for sign up, set default keyboard to email, hide passwords, padding when keyboard slides up. all of this. awesome nmeenda hivi na come

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, TextInput } from 'react-native';
+import { StyleSheet, View, TextInput, TouchableOpacity, Text } from 'react-native';
 
 export default class LoginForm extends Component {
   render() {
@@ -7,12 +7,22 @@ export default class LoginForm extends Component {
       <View style={styles.container}>
         <TextInput
           placeholder="username or password"
+          returnKeyType='next'
+          onSubmitEditing={() => this.passwordInput.focus() }
+          keyboardType='email-address'
+          autoCapitalize='none'
           style={styles.input}
         />
         <TextInput
           placeholder="password"
+          returnKeyType='go'
+          secureTextEntry
           style={styles.input}
+          ref={(input) => this.passwordInput = input}
         />
+        <TouchableOpacity style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>LOGIN</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -25,7 +35,16 @@ const styles = StyleSheet.create({
   input: {
     height: 40,
     backgroundColor: 'rgba(255,255,255, 0.7)',
-    marginBottom: 20,
+    marginBottom: 10,
     paddingHorizontal: 10
+  },
+  buttonContainer: {
+    backgroundColor: '#FFF77B',
+    paddingVertical: 15
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#FFF',
+    fontSize: 15
   }
 });
